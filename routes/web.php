7979d9e02c2d66dd24ff4a4cc07f9a7e9c72c4e1
hoaithuong2002 <dgth2o2@gmail.com>
2021-03-16 11:13:14 +0700

    bai thuc hanh route

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+//    return view('welcome');
+     echo "<h2>This is Home page</h2>";
+});
+Route::get('/about',function (){
+    echo "<h2>This is About page</h2>";
+});
+Route::get('/contact',function (){
+    echo "<h2>This is Contact page</h2>";
+});
+Route::get('/user', function (Request $request){
+    return view('user',['name'=>'Masud Alam']);
+});
+Route::get('/user/{name}', function ($name) {
+    echo "<h2>User name is $name</h2>";
+});
+Route::get('/user-name/{name?}', function ($name = 'Sohel') {
+    echo "<h2>User name is $name</h2>";
+});
+Route::get('/home',[HomeController::class,'index']);
